92860f3b5251cd09740e599bb0bccd6e0bbf9ea4
Arturo Barba <arturo.barba@iesdonana.org>
2019-01-10 15:49:04 +0100

    Prubeas

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,9 +38,9 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Inicio', 'url' => ['/site/index']],
+            ['label' => 'Entrenamientos', 'url' => ['/site/entrenamiento']],
+            ['label' => 'My', 'url' => ['/site/my']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
